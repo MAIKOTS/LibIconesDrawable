7c3f2bbfbd643f87633fabcbb820752de9fb7f01
Maiko.T.S <maiko.t.s.ofc@gmail.com>
2026-09-03 17:45:32 -0300

    Atualizar o build.gradle.kts

--- icone-library/build.gradle.kts
+++ icone-library/build.gradle.kts
@@ -9,5 +9,29 @@ android {
 
     defaultConfig {
         minSdk = 21
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
     }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.12.0")
 }
